fca45ca3f8b1a26c1129c9fe89089301e2d8a349
MrCarbene <Muhammad_Fouda@proton.me>
2023-05-16 03:52:53 +0200

    feat: [style] add auth form style.

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,121 @@
+'use client';
+
+import Button from '@/app/components/Button';
+import Input from '@/app/components/inputs/input';
+import { useRouter } from 'next/navigation';
+import { useCallback, useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import AuthSocialButton from './AuthSocialButton';
+import { BsGoogle, BsGithub } from 'react-icons/bs';
+
+type Variant = 'LOGIN' | 'REGISTER';
+
+type Props = {};
+
+function AuthForm({}: Props) {
+  const router = useRouter();
+  const [variant, setVariant] = useState<Variant>('LOGIN');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toggleVariant = useCallback(() => {
+    if (variant === 'LOGIN') {
+      setVariant('REGISTER');
+    } else {
+      setVariant('LOGIN');
+    }
+  }, [variant]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    setIsLoading(true);
+
+    if (variant === 'REGISTER') {
+    }
+
+    if (variant === 'LOGIN') {
+    }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+  };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white dark:bg-black px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === 'REGISTER' && (
+            <Input
+              label="Name"
+              errors={errors}
+              id="name"
+              type="text"
+              register={register}
+              disabled={isLoading}
+            />
+          )}
+          <Input
+            label="Email"
+            errors={errors}
+            id="email"
+            type="email"
+            register={register}
+            disabled={isLoading}
+          />
+          <Input
+            label="Password"
+            errors={errors}
+            id="password"
+            type="password"
+            register={register}
+            disabled={isLoading}
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+            </Button>
+          </div>
+        </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white dark:bg-black px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton icon={BsGithub} onClick={() => {}} />
+            <AuthSocialButton icon={BsGoogle} onClick={() => {}} />
+          </div>
+        </div>
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500 items-center">
+          <div>
+            {variant === 'LOGIN'
+              ? 'New to Messenger?'
+              : 'Already have an account?'}
+          </div>
+          <div onClick={toggleVariant} className="underline cursor-pointer">
+            {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AuthForm;
