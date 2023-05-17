e4de28a49789d072a4d3cb664ad969a5b799d575
MrCarbene <Muhammad_Fouda@proton.me>
2023-05-17 02:28:53 +0200

    feat: add style for userList component

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,12 +1,20 @@
 'use client';
 
 import useRoutes from '@/app/hooks/useRoutes';
+import { User } from '@prisma/client';
 import { useState } from 'react';
+import Avatar from './Avatar';
 import DesktopItem from './DesktopItem';
 
-const DesktopSidebar = () => {
+interface DesktopSidebarProps {
+  currentUser: User;
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState(false);
+
+  console.log(currentUser);
 
   return (
     <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white dark:lg:bg-black lg:border-r-[1px] dark:lg:border-r-gray-600 lg:pb-4 lg:flex lg:flex-col justify-between">
@@ -23,6 +31,13 @@ const DesktopSidebar = () => {
             />
           ))}
         </ul>
+      </nav>
+      <nav className="mt-4 flex flex-col justify-between items-center">
+        <div
+          onClick={() => setIsOpen(true)}
+          className="cursor-pointer hover:opacity-75 transition">
+          <Avatar user={currentUser} />
+        </div>
       </nav>
     </div>
   );
